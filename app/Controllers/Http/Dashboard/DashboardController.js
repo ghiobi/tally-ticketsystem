c5898437063006769d5fa300d1a4db7d6a8dc699
Earthii <xiao_eric@hotmail.com>
2019-01-21 18:42:23 -0500

    #21 admin dashboard

--- app/Controllers/Http/Dashboard/DashboardController.js
+++ app/Controllers/Http/Dashboard/DashboardController.js
@@ -1,8 +1,33 @@
 'use strict'
 
 class DashboardController {
-  index({ view }) {
-    return view.render('dashboard.index')
+  async index({ view, auth }) {
+    const data = {
+      open: [
+        { text: 'Tally has really sped things up', author: 'Eric Xiao' },
+        { text: 'Lorem ipsum yeah?', author: 'Laurendy Lam' }
+      ],
+      inprogress: [
+        { text: 'Hey can we get this feature', author: 'Brian Vo' },
+        { text: 'Tally is the best', author: 'Nathan Chao' },
+        { text: 'Wow, this is really useful', author: 'Kevin Zhang' },
+        {
+          text: 'Can this be integrated with slack bots?',
+          author: 'Michel Chatmajian'
+        }
+      ],
+      closed: [{ text: 'Thanks Tally!', author: 'Justin Leger' }]
+    }
+    return Promise.all([
+      auth.user.hasRole('admin'),
+      auth.user.hasRole('owner')
+    ]).then((values) => {
+      if (values.includes(true)) {
+        return view.render('dashboard.admin', data)
+      } else {
+        return view.render('dashboard.main')
+      }
+    })
   }
 }
 
--- database/seeds/DemoSeeder.js
+++ database/seeds/DemoSeeder.js
@@ -11,6 +11,8 @@
 */
 
 const Organization = use('App/Models/Organization')
+const Role = use('App/Models/Role')
+const User = use('App/Models/User')
 
 class DemoSeeder {
   async run() {
@@ -19,11 +21,24 @@ class DemoSeeder {
       slug: 'tally'
     })
 
-    organization.users().create({
+    await Role.createMany([
+      { key: 'owner', display_name: 'Organization Owner' },
+      { key: 'admin', display_name: 'Organization Administrator' }
+    ])
+
+    const userData = {
       email: 'owner@tally.com',
       name: 'Owner',
       password: 'password'
-    })
+    }
+
+    const user = new User(userData)
+    user.fill(userData)
+
+    await organization.users().save(user)
+
+    await user.setRole('owner')
+    await user.setRole('admin')
   }
 }
 
