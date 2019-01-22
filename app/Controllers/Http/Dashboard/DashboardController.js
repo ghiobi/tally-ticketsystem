ab0b52197ff7c74bf2efff0761b55e0f03b9f746
Earthii <xiao_eric@hotmail.com>
2019-01-21 22:14:14 -0500

    Added variable for website fullscreen

--- app/Controllers/Http/Dashboard/DashboardController.js
+++ app/Controllers/Http/Dashboard/DashboardController.js
@@ -4,14 +4,15 @@ const Ticket = use('App/Models/Ticket')
 class DashboardController {
   async index({ view, auth }) {
     // TODO: Clean this part up once API is available
-    const tickets = await Ticket.all()
+    const tickets = await Ticket.query()
+      .with('user')
+      .fetch()
     const data = {
       open: [],
       inprogress: [],
       closed: []
     }
-    for (const ticket of tickets.rows) {
-      ticket.author = await ticket.user().fetch()
+    for (const ticket of tickets.toJSON()) {
       if (ticket.status == 'submitted') {
         data['open'].push(ticket)
       } else if (ticket.status == 'replied') {
@@ -21,16 +22,10 @@ class DashboardController {
       }
     }
 
-    return Promise.all([
-      auth.user.hasRole('admin'),
-      auth.user.hasRole('owner')
-    ]).then((values) => {
-      if (values.includes(true)) {
-        return view.render('dashboard.admin', data)
-      } else {
-        return view.render('dashboard.main')
-      }
-    })
+    if (await auth.user.hasRole('admin')) {
+      return view.render('dashboard.admin', data)
+    }
+    return view.render('dashboard.main')
   }
 }
 
