481f6203baa864589640bf2387e01cb0b870cd32
Earthii <xiao_eric@hotmail.com>
2019-01-30 18:13:25 -0500

    #76 reply in frontend

--- app/Controllers/Http/Feedback/FeedbackController.js
+++ app/Controllers/Http/Feedback/FeedbackController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Ticket = use('App/Models/Ticket')
+const Message = use('App/Models/Message')
 
 class FeedbackController {
   async index({ view, request }) {
@@ -13,6 +14,35 @@ class FeedbackController {
 
     const data = { ticket: ticket.toJSON() }
     return view.render('feedback.main', data)
+  }
+
+  async reply({ response, request, auth }) {
+    const { feedback_id } = request.params
+
+    const ticket = await Ticket.query()
+      .where('id', feedback_id)
+      .with('messages.user') // returns messages linked to this ticket
+      .with('user') // returns who submited the ticket
+      .first()
+
+    const reply = request.input('reply', '')
+
+    //check ownership
+    if (ticket.user.id !== auth.user.id) {
+      if (!(await auth.user.hasRole('admin'))) {
+        return response.redirect('403')
+      }
+    }
+
+    Message.create({
+      user_id: auth.user.id,
+      ticket_id: ticket.id,
+      body: reply
+    })
+
+    return response.redirect(
+      `/organization/${request.organization.slug}/feedback/${feedback_id}`
+    )
   }
 }
 
