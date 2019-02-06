1a5d5720b2a7a828e0554f88c617b95ba869d09d
Nathan Chao <n2chao@gmail.com>
2019-02-05 19:15:41 -0500

    #47 add ticket info to the reponse

--- app/Controllers/Http/Ticket/MessageController.js
+++ app/Controllers/Http/Ticket/MessageController.js
@@ -4,13 +4,11 @@ const Ticket = use('App/Models/Ticket')
 
 class MessageController {
   async getTicketMessages({ response, params }) {
-    let ticket = await Ticket.find(params.ticketId)
-
     return response.json(
-      await ticket
-        .messages()
-        .with('user')
-        .fetch()
+      await Ticket.query()
+        .with('messages.user')
+        .where('id', params.ticketId)
+        .first()
     )
   }
 }
