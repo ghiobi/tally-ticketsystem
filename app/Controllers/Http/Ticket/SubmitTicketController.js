d0747f3ce1c72b7e01bc6ec4f181ac5b6a112d34
Kevin Zhang <izkevin52@gmail.com>
2019-02-09 15:34:04 -0500

    [Story #13][Task #112 #113 #124] Completed tests and implemetation of ticket submission

--- app/Controllers/Http/Ticket/SubmitTicketController.js
+++ app/Controllers/Http/Ticket/SubmitTicketController.js
@@ -2,18 +2,25 @@
 
 const Ticket = use('App/Models/Ticket')
 const Message = use('App/Models/Message')
-const InternalServerErrorException = use(
-  'App/Exceptions/InternalServerErrorException'
-)
 
 class SubmitTicketController {
   async index({ view }) {
     return view.render('ticket.SubmitPage')
   }
 
-  async submit({ response, request, auth }) {
+  async submit({ response, request, auth, session }) {
     const title = request.input('title')
     const body = request.input('body')
+
+    if (!title) {
+      session.flash({ notification: 'Missing title' })
+      return response.redirect('back')
+    }
+
+    if (!body) {
+      session.flash({ notification: 'Missing body' })
+      return response.redirect('back')
+    }
 
     const user = auth.user
 
@@ -23,15 +30,11 @@ class SubmitTicketController {
       status: 'submitted'
     })
 
-    if (ticket) {
-      const message = await Message.create({
-        user_id: user.id,
-        ticket_id: ticket.id,
-        body: body
-      })
-    } else {
-      throw new InternalServerErrorException()
-    }
+    await Message.create({
+      user_id: user.id,
+      ticket_id: ticket.id,
+      body: body
+    })
 
     return response.redirect('back')
   }
---
+++ test/functional/controllers/submit-ticket-controller.spec.js
@@ -0,0 +1,103 @@
+'use strict'
+
+const { test, trait, before } = use('Test/Suite')('Submit Ticket Controller')
+const {
+  OrganizationFactory,
+  UserFactory,
+  TicketFactory,
+  Message,
+  Ticket
+} = models
+
+trait('Test/ApiClient')
+trait('Auth/Client')
+trait('Session/Client')
+
+let organization = null
+let user = null
+let ticket = null
+
+before(async () => {
+  organization = await OrganizationFactory.create({
+    slug: 'submit-ticket-controller-test'
+  })
+
+  user = await UserFactory.make({
+    email: 'user-submit-ticket@email.com',
+    password: 'password'
+  })
+
+  await organization.users().save(user)
+
+  ticket = await TicketFactory.make()
+  await ticket.user().associate(user)
+})
+
+test('Ensure ticket is created after user submits new ticket', async ({
+  client,
+  assert
+}) => {
+  await client
+    .post(`organization/${organization.slug}/submit/ticket`)
+    .send({ title: 'Test ticket title', body: 'Random ticket message' })
+    .loginVia(user)
+    .end()
+
+  const ticket = await Ticket.query()
+    .where('title', 'Test ticket title')
+    .first()
+
+  assert.exists(ticket)
+})
+
+test('Ensure message is created after user submits new ticket', async ({
+  client,
+  assert
+}) => {
+  await client
+    .post(`organization/${organization.slug}/submit/ticket`)
+    .send({ title: 'Random Title', body: 'foobar' })
+    .loginVia(user)
+    .end()
+
+  const message = await Message.query()
+    .where('body', 'foobar')
+    .where('user_id', user.id)
+    .first()
+
+  assert.isNotNull(message)
+})
+
+test('Ensure ticket is not created if missing title ', async ({
+  client,
+  assert
+}) => {
+  await client
+    .post(`organization/${organization.slug}/submit/ticket`)
+    .send({ body: 'dfaf89s7D(*Adus980D9SAFKNAD;VKDAFA]}{PP{' })
+    .loginVia(user)
+    .end()
+
+  const message = await Message.query()
+    .where('body', 'dfaf89s7D(*Adus980D9SAFKNAD;VKDAFA]}{PP{')
+    .first()
+
+  assert.notExists(message)
+})
+
+test('Ensure ticket is not created if missing message body', async ({
+  client,
+  assert
+}) => {
+  await client
+    .post(`organization/${organization.slug}/submit/ticket`)
+    .send({ title: 'testTicket1' })
+    .loginVia(user)
+    .end()
+
+  const ticket = await Ticket.query()
+    .where('title', 'testTicket1')
+    .first()
+
+  assert.notExists(ticket)
+})
