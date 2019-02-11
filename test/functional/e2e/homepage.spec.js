7956c8a8902912c2d923cf9955649d196ec993d5
miisaka <digigundam@hotmail.com>
2019-02-10 22:23:29 -0500

    #44 Integration tests for organization login

--- test/functional/e2e/homepage.spec.js
+++ test/functional/e2e/homepage.spec.js
@@ -1,10 +1,32 @@
 'use strict'
 
-const { test, trait } = use('Test/Suite')('Homepage')
+const { test, trait, before } = use('Test/Suite')('Homepage integration test')
 
-trait('Test/Browser')
+const { OrganizationFactory, UserFactory, TicketFactory } = models
+
+trait('Test/Browser', {
+  defaultViewport: {
+    width: 1280,
+    height: 720,
+    isMobile: false
+  },
+  headless: true,
+  slowMo: 100
+})
 
 test('Visit home page', async ({ browser }) => {
   const page = await browser.visit('/')
   await page.assertHas('Tally, A Bot')
-})
+}).timeout(0)
+
+test('Clicking "Sign in" on homepage redirects to organization sign in page', async ({
+  browser
+}) => {
+  const page = await browser.visit('/')
+  await page
+    .waitForElement('#organization-signin')
+    .click('#organization-signin')
+    .waitFor(500)
+    .assertPath('/organization')
+    .assertHas('SIGN INTO YOUR ORGANIZATION')
+}).timeout(0)
