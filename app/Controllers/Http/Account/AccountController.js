4252a4cccae8f05211c8a1ffc79be3e1bcf51974
Earthii <xiao_eric@hotmail.com>
2019-02-21 19:28:45 -0500

    #143 Change password

---
+++ app/Controllers/Http/Account/AccountController.js
@@ -0,0 +1,36 @@
+'use strict'
+/** @type {import('@adonisjs/framework/src/Hash')} */
+const { validateAll } = use('Validator')
+
+class AccountController {
+  index({ view }) {
+    return view.render('account.index')
+  }
+
+  async password({ auth, session, request, response }) {
+    const validation = await validateAll(request.post(), {
+      newPassword: 'required|min:6'
+    })
+
+    var { newPassword, confirmPassword } = request.only([
+      'newPassword',
+      'confirmPassword'
+    ])
+
+    if (newPassword !== confirmPassword) {
+      session.flash({ error: 'Passwords are not the same' })
+    } else {
+      if (validation.fails()) {
+        session.flash({ error: 'Passwords needs to be 6 in length' })
+        return response.redirect('back')
+      }
+
+      auth.user.password = newPassword
+      await auth.user.save()
+    }
+
+    return response.redirect('back')
+  }
+}
+
+module.exports = AccountController
