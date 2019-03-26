f789e135184872351a718e54fc93599cafda5673
Earthii <xiao_eric@hotmail.com>
2019-03-25 20:15:59 -0400

    #191 refactor admin confirmation modal to use generic modal component

--- app/Controllers/Http/Admin/ManageAdminsController.js
+++ app/Controllers/Http/Admin/ManageAdminsController.js
@@ -1,21 +1,36 @@
 'use strict'
 
 const User = use('App/Models/User')
+const ForbiddenException = use('App/Exceptions/ForbiddenException')
 
 class ManageAdminsController {
-  async addAdmin({ request, response, session }) {
-    const { input_user_id } = request.post()
-    const user = await User.find(input_user_id)
+  async addAdmin({ request, response, session, auth }) {
+    if (!(await auth.user.hasRole('owner'))) {
+      throw new ForbiddenException()
+    }
+
+    const { modal_data } = request.post()
+
+    const user = await User.find(modal_data)
     await user.setRole('admin')
+
     session.flash({ success: `${user.name} now has admin privileges.` })
+
     return response.redirect('back')
   }
 
-  async removeAdmin({ request, response, session }) {
-    const { input_user_id } = request.post()
-    const user = await User.find(input_user_id)
+  async removeAdmin({ request, response, session, auth }) {
+    if (!(await auth.user.hasRole('admin'))) {
+      throw new ForbiddenException()
+    }
+
+    const { modal_data } = request.post()
+
+    const user = await User.find(modal_data)
     await user.removeRole('admin')
+
     session.flash({ success: `${user.name}'s admin privileges have been revoke.` })
+
     return response.redirect('back')
   }
 }
